fcb1c5a3e373ce6c5a4c75f098ac60dd109ee35d
Szymon Kasprzycki <66119938+Szymon-Kasprzycki@users.noreply.github.com>
2020-10-19 00:45:05 +0200

    First upload
    Started from computer version. 
    I first made header and pop-up menu sliding from the right side.

---
+++ js/menu.js
@@ -0,0 +1,23 @@
+function menu() {
+    const menuAppend = document.querySelector('.fa-bars');
+    const menuRemove = document.querySelector('.fa-angle-right');
+    const menu = document.querySelector('nav');
+
+    menuAppend.addEventListener('click', function () {
+        function append() {
+            menu.classList.add('active');
+            menuAppend.classList.add('hide');
+        }
+        setTimeout(append, 200)
+    })
+
+
+    menuRemove.addEventListener('click', function () {
+        menu.classList.remove('active');
+
+        function hide() {
+            menuAppend.classList.remove('hide')
+        }
+        setTimeout(hide, 800)
+    })
+}
